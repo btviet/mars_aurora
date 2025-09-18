c5c68acf0483d436f47d5f55eabc6148fb4655a7
Shayla Viet <sbviet@uio.no>
2025-09-18 11:09:41 +0200

    add color variables

--- parameters.py
+++ parameters.py
@@ -3,3 +3,11 @@ FONTSIZE_AXES_LABELS = 16
 FONTSIZE_AXES_TICKS = 14
 FONTSIZE_TITLE=18
 
+color_ion_low = '#4daf4a' #"green"
+color_ion_high = '#377eb8' #"blue"
+color_electron_low = '#ff7f00' #"orange"
+color_electron_high = '#e41a1c' #"red"
+colors_list = [color_ion_low, 
+                color_ion_high, 
+                color_electron_low, 
+                color_electron_high]
