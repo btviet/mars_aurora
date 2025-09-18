1284b9adf1e6597f714bc1a58e6ff908d3e91765
Shayla Viet <sbviet@uio.no>
2025-09-18 13:43:43 +0200

    calculate scaling factor v2

--- plot_integrated_electrons.py
+++ plot_integrated_electrons.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 from datetime import datetime
@@ -209,6 +210,155 @@ def calculate_scaling_factor():
     prediction_may_rayleighs =  march_scaling_factor*interpolated_sum_value_may
     print(f'Prediction for May Rayleighs: {prediction_may_rayleighs}')
 
+
+
+def calculate_scaling_factor_v2():
+    """
+    For every channel, multiply by bin width and bin center before summing
+    """
+    
+    may_rayleighs=50
+    march_rayleighs=100
+
+    # May event
+    print(f'May observation time {obs_times[4]}')
+    obs_start =  obs_times[4] - pd.Timedelta(hours=5)
+    obs_end = obs_times[4] + pd.Timedelta(hours=5)
+    filename = 'maven_f_flux_hr_aurora_attempt_5'
+    may_df =  read_maven_sep_flux_data(filename) 
+    numeric_cols = may_df.select_dtypes(include='number').columns # only numeric columns
+    may_df[numeric_cols] = may_df[numeric_cols].mask(may_df[numeric_cols] > 999999)
+    indices= may_df[(may_df['datetime']>=obs_start) & (may_df['datetime'] <= obs_end)].index
+    closest_obs_sample_index = indices[-1]
+    #index_range = [closest_obs_sample_index -24*5, closest_obs_sample_index +24*5+1]
+    sliced_df = may_df.iloc[indices].reset_index()
+    df_electron_flux = sliced_df.iloc[:,32:-2]
+    df_ion_flux = sliced_df.iloc[:,4:32]
+    df_datetime = sliced_df["datetime"]
+    df_electron_may = pd.concat([df_datetime, df_electron_flux], axis=1)
+    df_ion_may = pd.concat([df_datetime, df_ion_flux], axis=1)
+
+    df_electron_may.set_index('datetime', inplace=True)
+    df_electron_may = df_electron_may.iloc[:, ::-1]
+        
+    df_ion_may.set_index('datetime', inplace=True)
+    df_ion_may = df_ion_may.iloc[:, ::-1]    
+
+
+    bin_center_list = []
+    print("Columns: ", df_electron_may.columns)
+    for energy_bin in df_electron_may.columns:
+        #print("energy_bin: ", type(energy_bin), energy_bin)
+
+        boundaries = energy_bin[:-6].split('-')
+        #print(boundaries)
+        lower_boundary = float(boundaries[0])
+        upper_boundary = float(boundaries[1])
+        bin_width = upper_boundary-lower_boundary
+        bin_center = round(np.average([lower_boundary, upper_boundary]),3)
+        bin_center_list.append(str(bin_center))
+
+        df_electron_may[str(bin_center)] = df_electron_may[energy_bin]*bin_width*bin_center
+
+
+    df_electron_may.to_csv("files/integrated_electron_channels_may.csv", sep="\t")
+    df_electron_may = df_electron_may[bin_center_list]
+
+    df_electron_may["sum"] = df_electron_may.sum(axis=1)
+    df_electron_may.reset_index(inplace=True)
+
+    print(df_electron_may[["datetime", "sum"]])
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(df_electron_may["datetime"], df_electron_may["sum"], '-o', label='summed electron fluxes')
+    ax1.axvline(obs_times[4], color='black', linestyle='dashed', label='observation time')
+    ax1.legend()
+    plt.show()
+
+    # looks linear to me
+
+    diff = df_electron_may["sum"].iloc[5]-df_electron_may["sum"].iloc[4]
+    diff_per_minute = diff/60 
+    interpolated_sum_value_may = df_electron_may["sum"].iloc[4]+diff_per_minute*33
+    print(f'Interpolated value of sum at 00:33: {interpolated_sum_value_may}')
+
+    may_scaling_factor = may_rayleighs/interpolated_sum_value_may
+    print(f'Scaling factor May: {may_scaling_factor}')
+
+    # March event
+    print(f'March observation time {obs_times[3]}')
+    obs_start =  obs_times[3] - pd.Timedelta(hours=5)
+    obs_end = obs_times[3] + pd.Timedelta(hours=5)
+    filename = 'maven_f_flux_hr_aurora_attempt_4'
+    march_df =  read_maven_sep_flux_data(filename) 
+    numeric_cols = march_df.select_dtypes(include='number').columns # only numeric columns
+    march_df[numeric_cols] = march_df[numeric_cols].mask(march_df[numeric_cols] > 999999)
+    indices= march_df[(march_df['datetime']>=obs_start) & (march_df['datetime'] <= obs_end)].index
+
+    sliced_df = march_df.iloc[indices].reset_index()
+    df_electron_flux = sliced_df.iloc[:,32:-2]
+    df_ion_flux = sliced_df.iloc[:,4:32]
+    df_datetime = sliced_df["datetime"]
+    df_electron_march = pd.concat([df_datetime, df_electron_flux], axis=1)
+    df_ion_march = pd.concat([df_datetime, df_ion_flux], axis=1)
+
+    df_electron_march.set_index('datetime', inplace=True)
+    df_electron_march = df_electron_march.iloc[:, ::-1]
+        
+    df_ion_march.set_index('datetime', inplace=True)
+    df_ion_march = df_ion_march.iloc[:, ::-1]
+
+    bin_center_list = []
+    for energy_bin in df_electron_march.columns:
+        boundaries = energy_bin[:-6].split('-')
+        lower_boundary = float(boundaries[0])
+        upper_boundary = float(boundaries[1])
+        bin_width = upper_boundary-lower_boundary
+        bin_center = round(np.average([lower_boundary, upper_boundary]),3)
+        bin_center_list.append(str(bin_center))
+
+        df_electron_march[str(bin_center)] = df_electron_march[energy_bin]*bin_width*bin_center
+
+    df_electron_march = df_electron_march[bin_center_list]
+    df_electron_march["sum"] = df_electron_march.sum(axis=1)
+    df_electron_march.reset_index(inplace=True)
+
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(df_electron_march["datetime"], df_electron_march["sum"], '-o', label='summed electron fluxes')
+    ax1.axvline(obs_times[3], color='black', linestyle='dashed', label='observation time')
+    ax1.legend()
+    plt.show()
+    print(df_electron_march[["datetime", "sum"]])
+
+    diff = df_electron_march["sum"].iloc[5]-df_electron_march["sum"].iloc[4]
+    diff_per_minute = diff/60 
+    interpolated_sum_value_march = df_electron_march["sum"].iloc[4]+diff_per_minute*45
+    print(f'Interpolated value of sum at 06:45: {interpolated_sum_value_march}')
+
+    march_scaling_factor = march_rayleighs/interpolated_sum_value_march
+    print(f'Scaling factor March: {march_scaling_factor}')
+    
+    print("-------------------------")
+    prediction_may_rayleighs =  march_scaling_factor*interpolated_sum_value_may
+    print(f'Prediction for May Rayleighs: {prediction_may_rayleighs}')
+
 if __name__ == "__main__":
-    plot_integrated_electron_channels()
-    calculate_scaling_factor()
+
+    """
+    For every channel, multiply by bin width and bin center before summing
+
+    Also try out different electron channel ranges
+
+    Also check with the non-detections
+
+    
+    interpolation: check the point before and point after
+    # see which fit would be the best
+    # loglinear vs linear
+
+    
+    """
+    # plot_integrated_electron_channels()
+    # calculate_scaling_factor()
+    calculate_scaling_factor_v2()
